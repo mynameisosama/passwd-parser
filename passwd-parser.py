5eb9b0a610aee096172607ebf6ff30e506fc5619
mynameisosama <osama.aftab.1811@gamil.com>
2019-02-24 22:06:49 -0600

    Fixed pylint issues
    Signed-off-by: mynameisosama <osama.aftab.1811@gamil.com>

--- passwd-parser.py
+++ passwd-parser.py
@@ -1,4 +1,10 @@
-import os, sys, traceback, json, logging, argparse
+"""
+The following python script correlates data between user and groups of a linux system
+"""
+import os
+import json
+import logging
+import argparse
 
 
 DEFAULT_PASSWD_FILE = "/etc/passwd"
@@ -18,14 +24,14 @@ def parse_group_data(_file):
     :param _file: file pointer to read
     :return: name, group id, users
     """
-    while(True):
+    while True:
         _data = _file.readline()
-        if(not _data):
+        if not _data:
             break
         try:
             _d = _data.split(':')
             yield _d[0], _d[2], _d[3][:-1].split(',')
-        except(IndexError):
+        except IndexError:
             raise FormatError("Invalid Format for Groups File")
 
 
@@ -35,91 +41,91 @@ def parse_user_data(_file):
     :param _file: file pointer to read
     :return: name, user id, group id, description
     """
-    while(True):
+    while True:
         _data = _file.readline()
-        if(not _data):
+        if not _data:
             break
         try:
             _d = _data.split(':')
             yield _d[0], _d[2], _d[3], _d[4]
-        except(IndexError):
+        except IndexError:
             raise FormatError("Invalid Format for Passwd File")
 
 
-def parse_user_file(_file, groups_by_id, users):
+def parse_user_file(_file, _groups_by_id, _users):
     """
     Parse user file and populate users dictionary
     :param _file: path to passwd file
-    :param groups_by_id: dictionary of groups keyed by respective id
-    :param users: dictionary of users keyed by username
+    :param _groups_by_id: dictionary of groups keyed by respective id
+    :param _users: dictionary of users keyed by username
     """
     try:
-        with open(_file) as f:
+        with open(_file) as _fp:
             # extract parameters from each line
-            for u_name, u_uid, u_gid, u_info in parse_user_data(f):
+            for u_name, u_uid, u_gid, u_info in parse_user_data(_fp):
                 # check if extracted group id has an associated group present
-                if u_gid in groups_by_id:
+                if u_gid in _groups_by_id:
                     # add group if its not the same as username
                     # by default each user has a group with the same name
-                    if u_name != groups_by_id[u_gid]:
-                        users[u_name] = {
+                    if u_name != _groups_by_id[u_gid]:
+                        _users[u_name] = {
                             'uid': u_uid,
                             'full_name': u_info,
-                            'groups': [groups_by_id[u_gid]]
+                            'groups': [_groups_by_id[u_gid]]
                         }
                     else:
-                        users[u_name] = {
+                        _users[u_name] = {
                             'uid': u_uid,
                             'full_name': u_info,
                             'groups': list()
                         }
-                # incase a user is found whose group id 
+                # incase a user is found whose group id
                 # does not have asscoiated group
                 else:
-                    users[u_name] = {
+                    _users[u_name] = {
                         'uid': u_uid,
                         'full_name': u_info,
                         'groups': list()
                     }
-    except(KeyError, IndexError, IOError) as e:
-        raise(e)
+    except(KeyError, IndexError, IOError) as exc:
+        raise exc
 
 
-def parse_group_file(_file, groups_by_id, groups_by_user):
+def parse_group_file(_file, _groups_by_id, _groups_by_user):
     """
     Parse group file and populate users groups_by_id and groups_by_user
     :param _file: path to group file
-    :param groups_by_id: dictionary of groups keyed by respective id
-    :param groups_by_user: dictionary of groups keyed by username
+    :param _groups_by_id: dictionary of groups keyed by respective id
+    :param _groups_by_user: dictionary of groups keyed by username
     """
     try:
-        with open(_file) as f:
+        with open(_file) as _fp:
             # extract parameters from each line
-            for g_name, g_id, g_users in parse_group_data(f):
+            for g_name, g_id, g_users in parse_group_data(_fp):
                 # make link between user to group instaed of group to user
                 for user in g_users:
-                    if user in groups_by_user:
-                        groups_by_user[user].append(g_name)
+                    if user in _groups_by_user:
+                        _groups_by_user[user].append(g_name)
                     else:
-                        groups_by_user[user] = [g_name]
+                        _groups_by_user[user] = [g_name]
                 # make association between group name and id
-                groups_by_id[g_id] = g_name
-    except(KeyError, IndexError, IOError) as e:
-        raise(e)
+                _groups_by_id[g_id] = g_name
+    except(KeyError, IndexError, IOError) as exc:
+        raise exc
 
 
-def correlate_users_groups(users, groups_by_user):
+def correlate_users_groups(_users, _groups_by_user):
     """
     Iterate through user dictionary and populate groups of each user
-    :param users: dictionary of users keyed by username
-    :param groups_by_user: dictionary of groups keyed by username
+    :param _users: dictionary of users keyed by username
+    :param _groups_by_user: dictionary of groups keyed by username
     """
     try:
-        for user in users:
-            if user in groups_by_user:
-                users[user]['groups'].extend(groups_by_user[user])
-    except(KeyError, IndexError) as e:
-        raise(e)
+        for user in _users:
+            if user in _groups_by_user:
+                _users[user]['groups'].extend(_groups_by_user[user])
+    except(KeyError, IndexError) as exc:
+        raise exc
 
 
 # Argument Parser
@@ -151,6 +157,6 @@ try:
     parse_user_file(args.passwd, groups_by_id, users)
     correlate_users_groups(users, groups_by_user)
     # Print output to stdout
-    print(json.dumps(users, indent=4))
-except(KeyError, IndexError, FormatError, IOError) as e:
-    logging.exception(e)
+    print json.dumps(users, indent=4)
+except(KeyError, IndexError, FormatError, IOError) as exc:
+    logging.exception(exc)
